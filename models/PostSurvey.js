9932994563311b089625c171767fdd2416147c8b
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-04-06 22:58:29 -0400

    Update PostSurvey.js

--- models/PostSurvey.js
+++ models/PostSurvey.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 const PostSurveySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: Number,
-  email: { type: String, required: true, unique: true },
-  gender: String,
-  address: String,
+  enjoyment: { type: String, required: true },
+  difficulty: { type: String, required: true },
+  aiHelpfulness: { type: String, required: true },
+  aiInteraction: { type: String, required: true },
+  creativity: { type: String, required: true },
+  feedback: { type: String, required: true },
+  improvements: { type: String, required: true },
+  agreeToTerms: { type: Boolean, required: true },
+  preSurveyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PreSurvey",
+    required: true,
+  },
 });
 
 const PostSurvey = mongoose.model("PostSurvey", PostSurveySchema);
